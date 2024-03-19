1fc015909a79855f0d7b99deb395528636b6f4b8
Allen Tan <tanallenbernard@gmail.com>
2024-03-19 23:55:40 +0800

    associate data attributes in innerHTML with the current index of this

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addBook = document.querySelector('.add');
 const modal = document.querySelector('.modal');
 const modalBackdrop = document.querySelector('.modal-backdrop');
 const form = document.getElementById('form');
+const remove = document.querySelector('.remove-button')
 const myLibrary = [];
 
 
@@ -13,40 +14,6 @@ addBook.onclick = function() {
 modalBackdrop.onclick = function() {
     modal.classList.add("hidden");
     modalBackdrop.classList.remove("on");
-}
-
-//object constructor
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read; //yes or no
-}
-
-//prototype function
-Book.prototype.info = function() {
-    const gridContainer = document.querySelector('#grid-container');
-
-    const bookInfo = document.createElement('div');
-    bookInfo.classList.add('book-info')
-
-    bookInfo.innerHTML = `
-        <div class="title-group">
-            <p class="book-title"><strong>${this.title}</strong></p>
-            <p class="author"><em>${this.author}</em></p>
-        </div>
-        <div class="details">
-            <div class="info-group">
-                <p class="pages"><span class="tag">Pages:   </span>${this.pages}</p>
-                <p class="read"><span class="tag">Read:   </span>${this.read}</p>
-            </div>
-            <div class="button-group">
-                <button class="read-button">Read</button>
-                <button class="remove-button">Remove</button>
-            </div>
-        </div>
-    `;
-    gridContainer.appendChild(bookInfo);
 }
 
 form.onsubmit = function(e) {
@@ -78,4 +45,42 @@ function viewLibrary() {
     myLibrary.forEach(book => {
         book.info();
     });
+}
+
+// remove.onclick = function() {
+//     s
+// }
+
+//object constructor
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read; //yes or no
+}
+
+//prototype function
+Book.prototype.info = function() {
+    const gridContainer = document.querySelector('#grid-container');
+
+    const bookInfo = document.createElement('div');
+    bookInfo.classList.add('book-info')
+
+    bookInfo.innerHTML = `
+        <div class="title-group">
+            <p class="book-title"><strong>${this.title}</strong></p>
+            <p class="author"><em>${this.author}</em></p>
+        </div>
+        <div class="details">
+            <div class="info-group">
+                <p class="pages"><span class="tag">Pages:   </span>${this.pages}</p>
+                <p class="read"><span class="tag">Read:   </span>${this.read}</p>
+            </div>
+            <div class="button-group">
+                <button class="read-button">Read</button>
+                <button class="remove-button" data-index = ${myLibrary.indexOf(this)}>Remove</button>
+            </div>
+        </div>
+    `;
+    gridContainer.appendChild(bookInfo);
 }
